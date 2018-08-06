bce2776400be85ae7be1a93febb4c0b70f30b18a
nao20010128nao <nao20010128@gmail.com>
2018-08-06 22:25:09 +0900

    失敗しても止めない

--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+set +e
 cat "$1" | grep -v "^#" | grep -v "^$" | while read line ; do
   echo "Patching $line"
-  wget -qO- "$line" | patch -p1
+  wget -qO- "$line" | patch -p1 || echo "Failed"
 done
