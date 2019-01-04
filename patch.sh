9b1c7a808178d72ca24529253eadb060d518d8b5
nao20010128nao <nao20010128@gmail.com>
2019-01-04 15:16:35 +0900

    修正

--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set +e
-find /patches | sort | tail -n+2 | while read line ; do
+find /patches -type f | sort | while read line ; do
   echo "Patching $line"
   ( patch -p1 < $line ) || echo "Failed"
 done
